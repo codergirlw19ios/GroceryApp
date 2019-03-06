b710a911bb202f05b667cc22fc9189d5b62c0908
Milan <shahmileez@gmail.com>
2019-03-06 17:31:30 -0600

    Shah_GroceryApp

--- GroceryApp/GroceryApp/GroceryTrip.swift
+++ GroceryApp/GroceryApp/GroceryTrip.swift
@@ -7,3 +7,22 @@
 
 import Foundation
 
+class GroceryTrip {
+    private var userBudget: Double
+    private var shoppingListDictionary:[GroceryItem:Bool]
+    private var cart:[GroceryItem]
+    private var taxRate: Double = 0.0
+    var totalCost:Double
+    
+    init(userBudget: Double,shoppingListArray:[GroceryItem],taxRate: Double) {
+        
+        self.userBudget = userBudget
+        self.taxRate = taxRate
+    
+    }
+    
+    
+    
+    
+    
+}
---
+++ GroceryApp/GroceryApp/GroceryTripError.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryTripError.swift
+//  GroceryApp
+//
+//  Created by Messaging on 3/6/19.
+//
+
+import Foundation
+
+
+enum GrocertyTripError {
+    case totalExceedBudget
+    case itemNotFound
+    case qtyExceedAmt
+    case qtyShortOfReqAmt
+    case zeroTaxRate
+}
