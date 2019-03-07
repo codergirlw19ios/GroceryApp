6a87a00c86bb1af25e23d05518ab236cf636ed85
Milan <shahmileez@gmail.com>
2019-03-06 18:36:23 -0600

    Shah_GroceryApp

--- GroceryApp/GroceryApp/GroceryTrip.swift
+++ GroceryApp/GroceryApp/GroceryTrip.swift
@@ -8,19 +8,37 @@
 import Foundation
 
 class GroceryTrip {
-    private var userBudget: Double
+    private var userBudget: Double = 0.0
     private var shoppingListDictionary:[GroceryItem:Bool]
-    private var cart:[GroceryItem]
+    private var cart:[GroceryItem] = []
     private var taxRate: Double = 0.0
-    var totalCost:Double
+    
+    var totalCost: Double {
+        let total = cart.reduce(0) {(result, next) -> Double in
+            return result + (next.cost! * Double(next.quantity))
+        }
+        
+        return total * (1.0 + taxRate)
+    }
+    
+    var balance:Double {return userBudget - totalCost}
     
     init(userBudget: Double,shoppingListArray:[GroceryItem],taxRate: Double) {
         
         self.userBudget = userBudget
         self.taxRate = taxRate
-    
+        
+        let tempShoppingList = Set<GroceryItem>(shoppingListArray)
+        shoppingListDictionary = Dictionary(uniqueKeysWithValues: tempShoppingList.map {(item: $0 , inCart: false) })
+        
     }
     
+    
+    
+    
+    
+    
+    //if (balance < 0) {throw GroceryTripError.totalExceedBudget}
     
     
     
