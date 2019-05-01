811354bd5f1dfd665d6b45a2e0b2e84210b20cb4
Amanda Rawls <22384389+amandarawls@users.noreply.github.com>
2019-05-01 02:58:20 -0500

    generic urlnetwork

---
+++ GroceryApp/GroceryApp/Networking/Query.swift
@@ -0,0 +1,8 @@
+//
+//  Query.swift
+//  GroceryApp
+//
+//  Created by Amanda Rawls on 5/1/19.
+//
+
+import Foundation
---
+++ GroceryApp/GroceryApp/Networking/URLNetwork.swift
@@ -0,0 +1,8 @@
+//
+//  URLNetwork.swift
+//  GroceryApp
+//
+//  Created by Amanda Rawls on 5/1/19.
+//
+
+import Foundation
